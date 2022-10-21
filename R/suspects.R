a3744db70fcf9a0cce499785a8799c3d7dbb0511
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2022-10-21 14:59:23 +0200

    Add fun templates

--- R/suspects.R
+++ R/suspects.R
@@ -1,3 +1,15 @@
-function <- suspects() {
+suspects <- function() {
   # Generate a list of suspects
+}
+
+suspects_add <- function(suspects, suspects_new){
+
+}
+
+suspects_remove <- function(suspects, suspects_old) {
+
+}
+
+suspects_view <- function(suspects, n) {
+  print(suspects, n = n)
 }
