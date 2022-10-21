19f66cd0c8f065a2a2e9bf00c5377058865da512
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2022-10-21 16:51:52 +0200

    Add fun to create suspect list

--- R/suspects.R
+++ R/suspects.R
@@ -1,5 +1,25 @@
-suspects <- function() {
-  # Generate a list of suspects
+#' Generate a list of suspects
+suspects_create <- function(suspects = NULL) {
+  # Check args
+  stopifnot("`suspects` must be a character." = is.character(suspects))
+  stopifnot("`suspects` must be of type vector." = is.vector(suspects))
+
+  # Only one list of suspects is allowed per project, recursively check for
+  # lists in project
+  if(file.exists("suspects.rds")){
+    stop("
+    A list of suspects already exists in this working directory.
+    Each project can only have one list.")
+  }
+
+  # Create suspects list
+  if(is.null(suspects)){
+    suspect_list <- tibble::tibble(suspects = character()) 
+  } else {
+    suspect_list <- tibble::tibble(suspects = suspects)
+  }
+
+  saveRDS(suspect_list, file = "suspects.rds")
 }
 
 suspects_add <- function(suspects, suspects_new){
@@ -13,3 +33,5 @@ suspects_remove <- function(suspects, suspects_old) {
 suspects_view <- function(suspects, n) {
   print(suspects, n = n)
 }
+
+rbind(suspect_list, suspects = c("hello"))
