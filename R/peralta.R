159aa044c6660029732aaeefd3cda3394776b5ee
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2022-10-22 19:05:31 +0200

    Add notes

--- R/peralta.R
+++ R/peralta.R
@@ -1,3 +1,6 @@
 peralta <- function() {
-  # Investigate the suspects
+  # 1. Investigate the suspects. Map across each suspect, and append information
+  #    into evidence. If no existinging evidence is found on disk, create a file.
+  # 2. Create a diff: compare latest evidence with existing evidence.
+  # 3. Print diff to console (e.g., updates or lack thereof)
 }
