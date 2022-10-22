ea17a2348b81f919bf913f6811846ef0ed5593b4
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2022-10-22 18:53:07 +0200

    Refactor - remove return values

--- R/investigate.R
+++ R/investigate.R
@@ -1,18 +1,12 @@
 scrape <- function(url) {
   stopifnot("`url` must be a string." = is.character(url))
-
-  html <-
-    polite::bow(url) |>
+  polite::bow(url) |>
     polite::scrape()
-
-  return(html)
 }
 
 extract <- function(url) {
-  links <- scrape(url) |>
+  scrape(url) |>
     rvest::html_elements("a") |>
     rvest::html_attr("href") |>
     grep(".xls$|.xlsx$|.csv$|.ods$|.geojson$|.shp$", x = _, value = TRUE)
-
-  return(links)
 }
