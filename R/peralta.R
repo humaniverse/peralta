7b8e42448c3a628bfa6d02f31bc7d047c2afb484
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2022-10-24 19:05:24 +0200

    compare evidence across timestamps

--- R/peralta.R
+++ R/peralta.R
@@ -5,43 +5,73 @@ peralta <- function() {
     stop("A list of suspects could not be found. Add suspects with `suspects_add()`.")
   }
 
-  # Step 1 - load existing evidence dataframe
+  evidence_new <- suspect_list |>
+    dplyr::mutate(timestamp = Sys.time()) |>
+    dplyr::mutate(evidence = lapply(suspect, investigate)) |>
+    tidyr::unnest(evidence)
+
   if (file.exists("evidence.rds")) {
     evidence <- readRDS("evidence.rds")
+
+    evidence_last <- evidence |>
+      dplyr::filter(timestamp == max(timestamp))
+
+    evidence_added <-
+      dplyr::anti_join(
+        evidence_new,
+        evidence_last,
+        by = c("suspect", "evidence")
+      ) |>
+      dplyr::pull(evidence)
+
+    print(paste0("Data added: ", evidence_added))
+
+    evidence_removed <-
+      dplyr::anti_join(
+        evidence_last,
+        evidence_new,
+        by = c("suspect", "evidence")
+      ) |>
+      dplyr::pull(evidence)
+
+    print(paste0("Data removed: ", evidence_removed))
+
+    dplyr::bind_rows(evidence, evidence_new) |>
+      saveRDS("evidence.rds")
   } else {
-    # print some kind of message informing user this is the first time they have
-    # checked for evidence on their suspects so no diffs will be returned.
+    message("
+    This is the first time Peralta has investigated the suspects for evidence.
+    Run this function again to see if any new evidence has emerged.
+    ")
+    saveRDS(evidence_new, "evidence.rds")
   }
-
-  # Step 2 - create new evidence dataframe
-  suspect_list |> 
-    dplyr::mutate(timestamp = Sys.time()) |> 
-    dplyr::mutate(evidence = lapply(suspect, investigate_fake, status = "unchanged"))
-
-
-
-  # 3. Print diff to console (e.g., updates or lack thereof). Use the pillar
-  #    package. Additions in Green. Removals in red. No change in black text.
 }
-library(tidyverse)
 
-suspect_list <- readRDS("suspects.rds")
+# investigate_fake <- function(url, status) {
+#   tibble::tribble(
+#     ~url, ~status, ~dataset,
+#     "suspect_1", "base", "dataset_1",
+#     "suspect_1", "base", "dataset_2",
+#     "suspect_1", "update", "dataset_2",
+#     "suspect_1", "update", "dataset_3",
+#     "suspect_2", "base", "dataset_4",
+#     "suspect_2", "update", "dataset_4",
+#     "suspect_2", "update", "dataset_5",
+#   ) |>
+#   dplyr::filter(url == {{ url }}) |>
+#   dplyr::filter(status == {{ status }}) |>
+#   dplyr::select(evidence = dataset)
+# }
 
-time_1 <- suspect_list |>
-  dplyr::mutate(timestamp = Sys.time()) |>
-  dplyr::mutate(evidence = lapply(suspect, investigate_fake, status = "base")) |> 
-  unnest(evidence)
+# readRDS("suspects.rds") |>
+#   dplyr::mutate(timestamp = Sys.time()) |>
+#   dplyr::mutate(evidence = lapply(suspect, investigate_fake, status = "base")) |>
+#   tidyr::unnest(evidence) |>
+#   saveRDS("evidence.rds")
 
-time_2 <- suspect_list |>
-  dplyr::mutate(timestamp = Sys.time()) |>
-  dplyr::mutate(evidence = lapply(suspect, investigate_fake, status = "update")) |> 
-  unnest(evidence)
+# evidence()
+# peralta()
 
-# Added
-dataset_added <- dplyr::anti_join(time_2, time_1, by = c("suspect", "evidence")) |> 
-  pull(evidence)
-# Removed
-dataset_removed <- dplyr::anti_join(time_1, time_2, by = c("suspect", "evidence")) |> 
-  pull(evidence)
-
-time_2
+suspects_add("https://www.england.nhs.uk/statistics/statistical-work-areas/hospital-discharge-data/")
+peralta()
+evidence()
