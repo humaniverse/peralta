284655868bf0479ccf230528cc6578079c4c56c8
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2022-11-16 12:13:25 +0100

    Refactor

--- R/investigate.R
+++ R/investigate.R
@@ -1,9 +1,14 @@
 investigate <- function(url) {
   stopifnot("`url` must be a string." = is.character(url))
-  polite::bow(url) |>
-    polite::scrape() |>
+
+  response <- polite::bow(url) |>
+    polite::scrape()
+
+  dom <- response |>
     rvest::html_elements("a") |>
-    rvest::html_attr("href") |>
+    rvest::html_attr("href")
+
+  data_urls <- dom |>
     grep(".xls$|.xlsx$|.csv$|.ods$|.geojson$|.shp$", x = _, value = TRUE) |>
     tibble::as_tibble_col(column_name = "evidence")
 }
