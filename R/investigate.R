11ec6712ab3ce33ec92cba2c85c14b2c35a0e6bf
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2022-11-16 13:21:57 +0100

    Add url regex check

--- R/investigate.R
+++ R/investigate.R
@@ -1,5 +1,11 @@
 investigate <- function(url) {
   stopifnot("`url` must be a string." = is.character(url))
+  stopifnot(
+    "`url` is not a valid URL. Ensure it starts with 'http[s]:'" = grepl(
+      pattern = "http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+",
+      x = url
+    )
+  )
 
   response <- polite::bow(url) |>
     polite::scrape()
